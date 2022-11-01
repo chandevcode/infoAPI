316cae30b8538185f84fbe026b6235cf0702f322
chan <baguschandradp@gmail.com>
2022-11-01 13:07:21 +0800

    add model

---
+++ Data/ContactsApiDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using infoApi.Models;
+using Microsoft.EntityFrameworkCore;
+namespace infoApi.Data
+
+{
+  public class ContactsApiDbContext : DbContext
+  {
+    public ContactsApiDbContext()
+    {
+    }
+
+    public ContactsApiDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Contact> Contacts { get; set; }
+  }
+}
+
---
+++ Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace infoApi.Models
+{
+  public class Contact
+  {
+    public Guid Id { get; set; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public long Phone { get; set; }
+    public string address { get; set; }
+  }
+}
+
